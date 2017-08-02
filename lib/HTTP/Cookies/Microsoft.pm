de5e294a2698499a10a96ca18123f9cc8c456b7d
Olaf Alders <olaf@wundersolutions.com>
2017-08-02 15:41:39 -0400

    Don't "use vars" for $VERSION

--- lib/HTTP/Cookies/Microsoft.pm
+++ lib/HTTP/Cookies/Microsoft.pm
@@ -2,9 +2,9 @@ package HTTP::Cookies::Microsoft;
 
 use strict;
 
-use vars qw(@ISA $VERSION);
+use vars qw(@ISA);
 
-$VERSION = "6.01";
+our $VERSION = "6.01";
 
 require HTTP::Cookies;
 @ISA=qw(HTTP::Cookies);
