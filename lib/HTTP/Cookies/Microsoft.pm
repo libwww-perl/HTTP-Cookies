544cdeb01b0ae51ffca7655cbedfb4aec160e1c8
Olaf Alders <olaf@wundersolutions.com>
2019-11-12 09:29:22 -0500

    increment $VERSION after 6.06 release

--- lib/HTTP/Cookies/Microsoft.pm
+++ lib/HTTP/Cookies/Microsoft.pm
@@ -2,7 +2,7 @@ package HTTP::Cookies::Microsoft;
 
 use strict;
 
-our $VERSION = '6.06';
+our $VERSION = '6.07';
 
 require HTTP::Cookies;
 our @ISA=qw(HTTP::Cookies);
--- lib/HTTP/Cookies/Netscape.pm
+++ lib/HTTP/Cookies/Netscape.pm
@@ -2,7 +2,7 @@ package HTTP::Cookies::Netscape;
 
 use strict;
 
-our $VERSION = '6.06';
+our $VERSION = '6.07';
 
 require HTTP::Cookies;
 our @ISA=qw(HTTP::Cookies);
